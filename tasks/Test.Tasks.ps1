33c988372acdc779a088cf15d8fdf3a2df43fcfa
Steffen Kampmann <steffen.kampmann@gmail.com>
2025-12-11 11:03:16 +0100

    fixed missing build

--- tasks/Test.Tasks.ps1
+++ tasks/Test.Tasks.ps1
@@ -1,24 +1,37 @@
 
+#region Build & Import
+
 task Test.Import.PsSqlClient -Jobs {
     Join-Path $PSScriptRoot .. .\PsSqlClient | Push-Location
     Invoke-Build Import
 }
+
 task Test.Import.PsSmo -Jobs {
     Join-Path $PSScriptRoot .. .\PsSmo | Push-Location
     Invoke-Build Import
 }
 
-task Test.PsSmo -Jobs Test.Import.PsSqlClient, {
+task Test.Import.PsDac -Jobs {
+    Join-Path $PSScriptRoot .. .\PsDac | Push-Location
+    Invoke-Build Import
+}
+
+#endregion
+#region Tests
+
+task Test.PsSmo -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, {
     Join-Path $PSScriptRoot .. PsSmo test | Push-Location
     Invoke-Pester
 }
 
-task Test.PsDac -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, {
+task Test.PsDac -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, Test.Import.PsDac, {
     Join-Path $PSScriptRoot .. PsDac test | Push-Location
     Invoke-Pester
 }
 
-task Test.PsDac.ex.Azure -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, {
+task Test.PsDac.ex.Azure -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, Test.Import.PsDac, {
     Join-Path $PSScriptRoot .. PsDac test | Push-Location
     Invoke-Pester -Configuration @{ Filter = @{ ExcludeTag = 'AzureSql' }}
 }
+
+#endregion
