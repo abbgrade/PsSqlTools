56dac9a43c443d2091d5c52f2f820f4d76ec9bc2
Steffen Kampmann <steffen.kampmann@gmail.com>
2025-12-11 10:43:10 +0100

    fixed test dependencies

--- tasks/Test.Tasks.ps1
+++ tasks/Test.Tasks.ps1
@@ -1,20 +1,24 @@
 
-task Test.Import.PsSqlClient {
+task Test.Import.PsSqlClient -Jobs {
     Join-Path $PSScriptRoot .. .\PsSqlClient | Push-Location
     Invoke-Build Import
 }
+task Test.Import.PsSmo -Jobs {
+    Join-Path $PSScriptRoot .. .\PsSmo | Push-Location
+    Invoke-Build Import
+}
 
-task Test.PsSmo Test.Import.PsSqlClient, {
+task Test.PsSmo -Jobs Test.Import.PsSqlClient, {
     Join-Path $PSScriptRoot .. PsSmo test | Push-Location
     Invoke-Pester
 }
 
-task Test.PsDac {
+task Test.PsDac -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, {
     Join-Path $PSScriptRoot .. PsDac test | Push-Location
     Invoke-Pester
 }
 
-task Test.PsDac.ex.Azure {
+task Test.PsDac.ex.Azure -Jobs Test.Import.PsSqlClient, Test.Import.PsSmo, {
     Join-Path $PSScriptRoot .. PsDac test | Push-Location
     Invoke-Pester -Configuration @{ Filter = @{ ExcludeTag = 'AzureSql' }}
 }
